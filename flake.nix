fd0eabf0f9c011738427e2598c9f37aba7d12ffd
mdarocha <git@mdarocha.pl>
2025-06-05 17:01:50 +0200

    Bring back signature stripping - it was needed after all...
    Also add GitHub workflows and Dependabot configuration

--- flake.nix
+++ flake.nix
@@ -1,8 +1,13 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     {
+      checks.x86_64-linux.example = let
+        flake = import ./example/flake.nix;
+        outputs = flake.outputs { inherit nixpkgs; nuget-packageslock2nix = self; };
+      in outputs.packages.x86_64-linux.default;
+
       lib = { system, name ? "project", lockfiles ? [] }:
         with builtins;
         let
@@ -15,12 +20,24 @@
           in
           filter (dep: (hasAttr "contentHash" dep) && (hasAttr "resolved" dep)) allDeps;
 
-          getNuget = { name, resolved, contentHash, ... }: pkgs.dotnetCorePackages.fetchNupkg {
+          getNuget = { name, resolved, contentHash, ... }: (pkgs.dotnetCorePackages.fetchNupkg {
             pname = name;
             version = resolved;
-            url = "https://www.nuget.org/api/v2/package/${name}/${resolved}";
             hash = "sha512-${contentHash}";
-          };
+          }).overrideAttrs (old: {
+            src = pkgs.fetchurl {
+              name = old.src.name;
+              url = old.src.url;
+              hash = "sha512-${contentHash}";
+
+              downloadToTemp = true;
+              postFetch = ''
+                mv $downloadedFile file.zip
+                ${pkgs.zip}/bin/zip -d file.zip ".signature.p7s"
+                mv file.zip $out
+              '';
+            };
+          });
         in
         map getNuget (concatMap (src: externalDeps (fromJSON (readFile src))) lockfiles);
     };
